1bfbe82df142b52afb17928b7c105e43fe62949a
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-02-22 16:05:12 -0300

    feat: Service layer, component registration

--- src/main/java/com/marcelinx/shop/resources/UserResource.java
+++ src/main/java/com/marcelinx/shop/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.marcelinx.shop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcelinx.shop.entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.marcelinx.shop.service.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-  @GetMapping
-  public ResponseEntity<User> findAll() {
-    User u1 = new User(1L, "Maria", "maria@gmail.com", "1111", "12345");
+	@Autowired 
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 
-    return ResponseEntity.ok().body(u1);
-  }
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
