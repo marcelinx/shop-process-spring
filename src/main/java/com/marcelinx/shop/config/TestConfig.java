5dc5a359b17dd49b1896063ba175a26028a5b3a2
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-02-27 15:40:40 -0300

    feat: adicionando Enum Order Status

--- src/main/java/com/marcelinx/shop/config/TestConfig.java
+++ src/main/java/com/marcelinx/shop/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.marcelinx.shop.entities.Order;
 import com.marcelinx.shop.entities.User;
+import com.marcelinx.shop.entities.enums.OrderStatus;
 import com.marcelinx.shop.repository.OrderRepository;
 import com.marcelinx.shop.repository.UserRepository;
 
@@ -28,11 +29,11 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "joao", "joao@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "beatryz", "beatryz@gmail.com", "977777777", "123456");
+		User u2 = new User(null, "beatryz", "alex@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
